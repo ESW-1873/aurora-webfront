a05fd5c427cae379bdb52924e41bf3fc25e81f1f
Shoya <touka.nt.gh@gmail.com>
2021-07-15 00:15:29 +0900

    add colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+export const black = '#000000'
+export const white = '#FFFFFF'
+export const gray = '#63666A'
+export const pink = '#F9529F'
+export const lightblue = '#1DB3EB'
+export const deeblue = '#6457A6'
+
+export type Colors =
+  | typeof black
+  | typeof white
+  | typeof gray
+  | typeof pink
+  | typeof lightblue
+  | typeof deeblue
