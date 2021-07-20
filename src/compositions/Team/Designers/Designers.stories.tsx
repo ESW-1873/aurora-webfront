af3becaf39f8887ef350eaeaee4da97e82e6f1d3
Shuto kikuchi <mizoren8@gmail.com>
2021-07-20 15:26:27 +0900

    update: storybook configs and styles

--- src/compositions/Team/Designers/Designers.stories.tsx
+++ src/compositions/Team/Designers/Designers.stories.tsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import { GlobalStyles } from 'src/styles/global-styles'
 import { Designers } from '.'
 
 export default {
   title: 'Team/Designers',
 }
 
-export const DesignersPage = () => <Designers />
+export const DesignersPage = () => (
+  <>
+    <GlobalStyles />
+    <Designers />
+  </>
+)
--- src/compositions/Team/Investors/Investors.stories.tsx
+++ src/compositions/Team/Investors/Investors.stories.tsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import { GlobalStyles } from 'src/styles/global-styles'
 import { Investors } from '.'
 
 export default {
   title: 'Team/Investors',
 }
 
-export const InvestorsPage = () => <Investors />
+export const InvestorsPage = () => (
+  <>
+    <GlobalStyles />
+    <Investors />
+  </>
+)
