0c7c4054ec3007c6c3a4670642bac9c1342c7453
Shoya <touka.nt.gh@gmail.com>
2021-07-23 04:47:11 +0900

    remove unnecessary

--- src/pages/disclaimer.tsx
+++ src/pages/disclaimer.tsx
@@ -1,12 +1,4 @@
-import { GetStaticProps } from 'next'
 import { Disclaimer } from 'src/compositions/documents/Disclaimer'
-
-export const getStaticProps: GetStaticProps = async () => {
-  const result = {
-    props: {},
-  }
-  return JSON.parse(JSON.stringify(result))
-}
 
 const DisclaimerPage = () => <Disclaimer />
 
