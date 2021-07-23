f1310b7bf2bf75e5e70f2416e3dc90dc4af59fed
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-07-24 01:27:23 +0900

    update sandbox page - use hooks for connecting Contract & add comments

--- src/pages/__sandbox.tsx
+++ src/pages/__sandbox.tsx
@@ -1,22 +1,39 @@
 import { BigNumber } from 'ethers'
 import { NextPage } from 'next'
 import { useEffect, useState } from 'react'
+import { useContract } from 'src/external/contract'
 import { useMetamask, useWallet, useWalletConnect } from 'src/external/wallet'
 import { useWalletStore } from 'src/stores'
 import { lightblue, white } from 'src/styles/colors'
 import styled from 'styled-components'
 
+/* custom hook - address入力 */
 const useAddress = () => {
-  const [address, setAddress] = useState('')
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setAddress(e.target.value)
+  const [walletAdr, setWalletAddress] = useState('')
+  const [fromAdr, setFromAddress] = useState('')
+  const [toAdr, setToAddress] = useState('')
+
+  const handleChangeWalletAdr = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setWalletAddress(e.target.value)
+  const handleChangeFromAdr = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setFromAddress(e.target.value)
+  const handleChangeToAdr = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setToAddress(e.target.value)
 
   return {
-    address,
-    handleChange,
+    walletAdr,
+    fromAdr,
+    toAdr,
+    handleChangeWalletAdr,
+    handleChangeFromAdr,
+    handleChangeToAdr,
   }
 }
 
+/*
+ * for DEBUG
+ * Singerから取得できる情報
+ */
 type SignerInfo = {
   address: string
   balance: BigNumber
@@ -24,15 +41,31 @@ type SignerInfo = {
   transactionCount: number
 }
 
+/*
+ * sandbox用page
+ * contract/wallet接続の検証
+ */
 const SandBox: NextPage = () => {
-  const { address, handleChange } = useAddress()
-  const { currentSigner } = useWalletStore()
+  const {
+    walletAdr,
+    fromAdr,
+    toAdr,
+    handleChangeWalletAdr,
+    handleChangeFromAdr,
+    handleChangeToAdr,
+  } = useAddress()
+
+  /* Wallet connection */
   const { disconnect } = useWallet()
   const connectMetamask = useMetamask()
   const connectWalletConnect = useWalletConnect()
+  /* Contract connection */
+  const { issue, stake, unstake, restake } = useContract()
 
+  // for DEBUG
+  // Signer(署名者)の状態を表示するため
+  const { currentSigner } = useWalletStore()
   const [signerInfo, setSignerInfo] = useState<SignerInfo | null>(null)
-
   useEffect(() => {
     async function fetchSignerInfo() {
       if (currentSigner !== null) {
@@ -52,18 +85,24 @@ const SandBox: NextPage = () => {
 
   return (
     <FlexBox>
-      <Spacer height={8} />
+      <h1>Wallet</h1>
       <FlexRow>
-        <h3>address</h3>
+        <h3>Wallet Address</h3>
         <Spacer width={8} />
-        <InputText type="text" value={address} onChange={handleChange} />
+        <InputText
+          type="text"
+          value={walletAdr}
+          onChange={handleChangeWalletAdr}
+        />
       </FlexRow>
-      <h1>useWallet</h1>
-      <Button onClick={() => connectMetamask({ address: address })}>
+      <Spacer height={8} />
+      <h2>useWallet</h2>
+      <Button onClick={() => connectMetamask({ address: walletAdr })}>
         connectMetamask
       </Button>
       <Button onClick={connectWalletConnect}>connectWalletConnect</Button>
       <Button onClick={() => disconnect()}>disconnect</Button>
+      <h3>signerInfo</h3>
       {signerInfo === null ? (
         <h3>null</h3>
       ) : (
@@ -74,6 +113,30 @@ const SandBox: NextPage = () => {
           <h3>{`getTransactionCount ... ${signerInfo.transactionCount}`}</h3>
         </>
       )}
+      <Spacer height={8} />
+      <h3>-------------------------------------</h3>
+      <Spacer height={8} />
+      <h1>Contract</h1>
+      <Spacer height={8} />
+      <h2>useContract</h2>
+      <Button onClick={issue}>issue</Button>
+      <Spacer height={8} />
+      <FlexRow>
+        <h3>To Address</h3>
+        <Spacer width={8} />
+        <InputText type="text" value={toAdr} onChange={handleChangeToAdr} />
+      </FlexRow>
+      <Spacer height={8} />
+      <Button onClick={() => stake(toAdr)}>stake</Button>
+      <Button onClick={() => unstake(toAdr)}>unstake</Button>
+      <Spacer width={8} />
+      <FlexRow>
+        <h3>From Address</h3>
+        <Spacer width={8} />
+        <InputText type="text" value={fromAdr} onChange={handleChangeFromAdr} />
+      </FlexRow>
+      <Spacer width={8} />
+      <Button onClick={() => restake(fromAdr, toAdr)}>restake</Button>
     </FlexBox>
   )
 }
