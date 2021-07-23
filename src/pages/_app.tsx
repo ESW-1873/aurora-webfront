9ac204e6adb292a8bde01dd5a42444bb0e9135e9
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-07-23 21:19:22 +0900

    add recoil root to _app.tsx for using state management by recoil

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { RecoilRoot } from 'recoil'
 import 'src/styles/fonts.css'
 import { GlobalStyles } from 'src/styles/global-styles'
 import 'src/styles/globals.css'
@@ -8,11 +9,16 @@ import 'src/styles/reset.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <RecoilRoot>
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </RecoilRoot>
     </>
   )
 }
