16f93308738fd2c1cb492df414f5c1917586bd0f
atatur9 <momodarou2015@gmail.com>
2021-07-28 22:43:29 +0900

    Revert "Merge pull request #7 from squardinc/add-related-blockchain"
    This reverts commit 56d3727192fee425db8663bc6d8823da16060130, reversing
    changes made to 925f920261e186bb38fd6bf4539bdc8652f7cfee.

--- src/external/index.ts
+++
@@ -1,3 +0,0 @@
-export { useExternal } from './common'
-export { HologramRelation, useContract } from './contract'
-export { useMetamask, useWalletConnect } from './wallet'
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { VFC } from 'react'
-import { RecoilRoot } from 'recoil'
 import 'src/styles/fonts.css'
 import { GlobalStyles } from 'src/styles/global-styles'
 import 'src/styles/globals.css'
@@ -12,19 +11,14 @@ const MyApp: VFC<AppProps> = ({ Component, pageProps, router: { asPath } }) => {
   const pageUrl = `${ROOT_URL}${asPath}`
   return (
     <>
-      <RecoilRoot>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <meta property="og:url" content={pageUrl} />
-          <link rel="canonical" href={pageUrl} />
-          {asPath.startsWith('app') && <meta name="robots" content="noindex" />}
-        </Head>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </RecoilRoot>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:url" content={pageUrl} />
+        <link rel="canonical" href={pageUrl} />
+        {asPath.startsWith('app') && <meta name="robots" content="noindex" />}
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
     </>
   )
 }
