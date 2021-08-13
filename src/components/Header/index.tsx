f874689b8e919e0acb7a8d712350bf6296324e38
Shuto kikuchi <mizoren8@gmail.com>
2021-08-13 16:06:07 +0900

    remove: unused header elements

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,51 +1,25 @@
-import { useRouter } from 'next/dist/client/router'
-import React, { ReactNode, VFC } from 'react'
+import React, { VFC } from 'react'
 import { HeaderButton } from 'src/components/Buttons'
 import { Logo } from 'src/components/Logo'
 import { Link } from 'src/elements/Link'
 import { headerHeight } from 'src/styles/mixins'
-import { APP, TOP } from 'src/utils/router'
-import { ellipsizeMid } from 'src/utils/string'
+import { TOP } from 'src/utils/router'
 import styled from 'styled-components'
-import { Navigation } from './Navigation'
 
-const HeaderWrapper: VFC<{ children: ReactNode } & HeaderLayoutStyleProps> = ({
-  children,
-  withBorder,
-}) => (
-  <>
-    <HeaderLayout withBorder={withBorder}>{children}</HeaderLayout>
-  </>
-)
 export const Header: VFC = () => {
   return (
     <>
-      <HeaderWrapper>
+      <HeaderLayout>
         <Link href={TOP}>
           <Logo />
         </Link>
         <HeaderButton label="Connect Wallet" onClick={() => {}} />
-      </HeaderWrapper>
+      </HeaderLayout>
     </>
   )
 }
 
-export const AppHeader: VFC = () => {
-  const { pathname } = useRouter()
-  // TODO fetch from context
-  const address = ellipsizeMid('0xfa3jxxxxxxxxxxxxxxoxa2', 6, 4)
-  return (
-    <HeaderWrapper withBorder>
-      <Link href={APP}>
-        <Logo iconOnly />
-      </Link>
-      <Navigation currentPath={pathname} address={address} />
-    </HeaderWrapper>
-  )
-}
-
-type HeaderLayoutStyleProps = { withBorder?: boolean }
-const HeaderLayout = styled.header<HeaderLayoutStyleProps>`
+const HeaderLayout = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
