235e058225e01ab7ce55499c9610f8e60cf26757
Shuto kikuchi <mizoren8@gmail.com>
2021-08-15 19:50:07 +0900

    update: layout wallet modal

--- src/components/Modal/WalletModal/WalletError.tsx
+++ src/components/Modal/WalletModal/WalletError.tsx
@@ -1,45 +1,29 @@
 import { UnsupportedChainIdError } from '@web3-react/core'
 import { VFC } from 'react'
-import { fontWeightBold, fontWeightMedium } from 'src/styles/font'
-import { breakpoint } from 'src/styles/mixins'
 import styled from 'styled-components'
+import { Heading, SubHeading } from '../common'
 
 export const WalletError: VFC<{ error: Error }> = ({ error }) => {
   const isUnsupportedChainIdError = error instanceof UnsupportedChainIdError
   return (
     <>
       <Layout>
-        <Title>{isUnsupportedChainIdError ? 'Wrong Network' : 'Error'}</Title>
-        <Description>
+        <Heading>
+          {isUnsupportedChainIdError ? 'Wrong Network' : 'Error'}
+        </Heading>
+        <SubHeading>
           {isUnsupportedChainIdError
-            ? 'Please connect to the Ethereum Mainnet.'
-            : 'Please try refreshing the page and connecting again.'}
-        </Description>
+            ? 'Please switch to Ethereum Mainnet'
+            : 'Please try refreshing the page and connecting again'}
+        </SubHeading>
       </Layout>
     </>
   )
 }
 
 const Layout = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  @media ${breakpoint.s} {
-    padding: 0 20px;
+  padding: 0 24px;
+  ${Heading} {
+    margin-bottom: 16px;
   }
-`
-
-const Title = styled.h3`
-  font-size: 32px;
-  font-weight: ${fontWeightBold};
-  line-height: calc(41 / 32);
-  margin-bottom: 32px;
-  text-align: center;
-`
-const Description = styled.p`
-  font-size: 16px;
-  font-weight: ${fontWeightMedium};
-  line-height: 1.4;
-  letter-spacing: 0.024em;
-  text-align: center;
 `
--- src/constants/misc.ts
+++ src/constants/misc.ts
@@ -1,1 +1,5 @@
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const METAMASK_DESCRIPTION = 'Easy-to-use browser extension.'
+export const WALLET_CONNECT_DESCRIPTION =
+  'Connect to Trust Wallet, Rainbow Wallet and more…'
