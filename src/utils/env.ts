c1e0fae93365ba99101f098923a847bb1b37f045
Shuto kikuchi <mizoren8@gmail.com>
2021-08-16 20:27:04 +0900

    fix: rename hologram to aurora

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -4,6 +4,6 @@ export const isProd = Boolean(process.env.NEXT_PUBLIC_IS_PROD)
 
 export const HOSTNAME = isProd
   ? SITE_SEO_DATA.siteDomain
-  : process.env.NEXT_PUBLIC_VERCEL_URL || 'dev.hologramdao.com'
+  : process.env.NEXT_PUBLIC_VERCEL_URL || 'dev.auroradao.org'
 
 export const ROOT_URL = `https://${HOSTNAME}`
