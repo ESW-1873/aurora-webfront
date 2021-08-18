36394152851c4857f9e5c504edcfa525ca86fad4
Shuto kikuchi <mizoren8@gmail.com>
2021-08-18 17:51:43 +0900

    add: comment

--- src/compositions/Post/index.tsx
+++ src/compositions/Post/index.tsx
@@ -14,6 +14,7 @@ import styled from 'styled-components'
 import { BlurredBackground } from './BlurredBackground'
 import { Contents } from './Contents'
 
+//TODO: ページ生成時にデータ注入。（ページロード時に取得する系のデータも一旦全部MOCKになってます）
 export const Post: VFC<SEOProps> = ({ ...seoProps }) => (
   <>
     <SEO {...seoProps} />
