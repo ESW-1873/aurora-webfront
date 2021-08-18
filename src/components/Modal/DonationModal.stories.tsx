6bc4545222076a9e8d57d30f235a1fafa56363f5
Shuto kikuchi <mizoren8@gmail.com>
2021-08-18 14:30:57 +0900

    fix: stories

--- src/components/Modal/DonationModal.stories.tsx
+++ src/components/Modal/DonationModal.stories.tsx
@@ -1,6 +1,6 @@
 import React, { VFC } from 'react'
 import { RecoilRoot } from 'recoil'
-import { isOpenedWalletModalAtom } from 'src/stores/Modal/walletModal'
+import { isOpenedDonateModalAtom } from 'src/stores/Modal/donateModal'
 import { GlobalStyles } from 'src/styles/global-styles'
 import { DonationModal } from './DonationModal'
 
@@ -12,7 +12,7 @@ const MOCK_TOTAL_DONATION = 12.32344
 
 export const Content: VFC = () => (
   <RecoilRoot
-    initializeState={(snap) => snap.set(isOpenedWalletModalAtom, true)}
+    initializeState={(snap) => snap.set(isOpenedDonateModalAtom, true)}
   >
     <GlobalStyles />
     <DonationModal totalDonation={MOCK_TOTAL_DONATION} />
