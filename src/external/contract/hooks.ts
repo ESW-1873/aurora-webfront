75fdb9c3bef2de6608cbae0b07bf780c5697f569
Shuto kikuchi <mizoren8@gmail.com>
2021-08-19 20:40:30 +0900

    add: contract hooks

---
+++ src/external/contract/hooks.ts
@@ -0,0 +1,56 @@
+import { ContractReceipt, ContractTransaction, utils } from 'ethers'
+import { useCallback } from 'react'
+import { useContractStore } from 'src/stores'
+
+// common logic for usecase related to using contract
+/** wrapper when call Contract methods  */
+async function call(
+  func: Promise<ContractTransaction>,
+): Promise<ContractReceipt | ContractTransaction | null> {
+  try {
+    const tx = await func
+    return tx
+  } catch (e) {
+    console.error(e)
+    throw e
+  }
+}
+/** overhead processing when no contract */
+function handleNoContract() {
+  console.error('need connection to contract') // TODO: error handling
+  return null
+}
+
+/**
+ * Consts for using Contract
+ * TODO: resetting
+ */
+const DEFAULT_GAS_LIMIT = 4500000
+
+/**
+ * Contractを利用するためのhooks
+ */
+export const useContract = () => {
+  const { contract } = useContractStore()
+
+  const donate = useCallback(
+    async (
+      postId: string,
+      amount: string,
+    ): Promise<ContractReceipt | ContractTransaction | null> => {
+      if (contract === null) return handleNoContract()
+      const parsedAmount = utils.parseEther(amount) // ether to wei
+      return call(
+        contract.donate(postId, {
+          value: parsedAmount,
+          gasLimit: DEFAULT_GAS_LIMIT,
+        }),
+      )
+    },
+    [contract],
+  )
+
+  return {
+    donate,
+  }
+}
