7b52d64d094eda6d278034ac7cff285d3a773f81
Shoya <touka.nt.gh@gmail.com>
2021-08-19 19:53:43 +0900

    enable to call graphql api from components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { VFC } from 'react'
 import { RecoilRoot } from 'recoil'
+import { publicApiClient } from 'src/api/client'
 import { getLibrary } from 'src/external'
 import { WalletInitializer } from 'src/initializers'
 import 'src/styles/fonts.css'
@@ -11,6 +12,7 @@ import 'src/styles/globals.css'
 import 'src/styles/reset.css'
 import { ROOT_URL } from 'src/utils/env'
 import { extractPathname } from 'src/utils/router'
+import { Provider as UrqlProvider } from 'urql'
 
 const MyApp: VFC<AppProps> = ({ Component, pageProps, router: { asPath } }) => {
   const pageUrl = `${ROOT_URL}${extractPathname(asPath)}`
@@ -19,16 +21,18 @@ const MyApp: VFC<AppProps> = ({ Component, pageProps, router: { asPath } }) => {
       <Web3ReactProvider getLibrary={getLibrary}>
         <RecoilRoot>
           <WalletInitializer>
-            <Head>
-              <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1.0"
-              />
-              <meta property="og:url" content={pageUrl} />
-              <link rel="canonical" href={pageUrl} />
-            </Head>
-            <GlobalStyles />
-            <Component {...pageProps} />
+            <UrqlProvider value={publicApiClient()}>
+              <Head>
+                <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1.0"
+                />
+                <meta property="og:url" content={pageUrl} />
+                <link rel="canonical" href={pageUrl} />
+              </Head>
+              <GlobalStyles />
+              <Component {...pageProps} />
+            </UrqlProvider>
           </WalletInitializer>
         </RecoilRoot>
       </Web3ReactProvider>
