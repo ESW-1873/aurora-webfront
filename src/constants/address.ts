3578de47ceb2c85eb075e2bd8d303245e6a68893
Shoya <touka.nt.gh@gmail.com>
2021-08-22 00:51:01 +0900

    update address

--- src/constants/address.ts
+++ src/constants/address.ts
@@ -4,6 +4,6 @@ type ChainInfo = { [chainId: number]: string }
 
 export const CONTRACT_ADDRESS: ChainInfo = {
   [SupportedChainId.MAINNET]: '', // TODO:
-  [SupportedChainId.RINKEBY]: '0x7dAE29479fE2Ebf4fd02D300F6dB603Bf26D505f', // TODO: 変更したら追従するようにする仕組みを検討
+  [SupportedChainId.RINKEBY]: '0xd3b43c83028A5CCFdCA2Ff122369eCC62AC8B8c0', // TODO: 変更したら追従するようにする仕組みを検討
   [SupportedChainId.GANACHE]: '', // SET YOUR CONTRACT ADDRESS
 }
