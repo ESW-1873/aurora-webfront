c691011b73a2189d98728a2db0f83fbd0c5ae959
Shoya <touka.nt.gh@gmail.com>
2021-08-21 23:30:27 +0900

    support light-mode favicon

--- src/components/Favicons/index.tsx
+++ src/components/Favicons/index.tsx
@@ -8,12 +8,7 @@ export const Favicons: VFC = () => (
       sizes="180x180"
       href="/favicons/apple-touch-icon.png"
     />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/favicons/favicon-32x32.png"
-    />
+    <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
     <link
       rel="icon"
       type="image/png"
