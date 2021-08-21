7a179ee687a87e7102ffc43ada2a5f5e59b0a9c6
Shoya <touka.nt.gh@gmail.com>
2021-08-21 16:45:49 +0900

    support ganache-cli chain

--- src/constants/address.ts
+++ src/constants/address.ts
@@ -5,4 +5,5 @@ type ChainInfo = { [chainId: number]: string }
 export const CONTRACT_ADDRESS: ChainInfo = {
   [SupportedChainId.MAINNET]: '', // TODO:
   [SupportedChainId.RINKEBY]: '0x3562F9D4C545448f0532cDD008455B1E13392A72', // TODO: 変更したら追従するようにする仕組みを検討
+  [SupportedChainId.GANACHE]: '', // SET YOUR CONTRACT ADDRESS
 }
