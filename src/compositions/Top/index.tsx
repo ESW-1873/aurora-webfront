fef050e9285049287f54bd90defe533dc0f902e8
Shuto kikuchi <mizoren8@gmail.com>
2021-08-24 18:28:49 +0900

    update: text

--- src/compositions/Top/index.tsx
+++ src/compositions/Top/index.tsx
@@ -1,16 +1,24 @@
 import React, { VFC } from 'react'
 import {
+  AFTER_PROJECT_ENDS_TEXT,
   CANCEL_TEXT,
   DONATION_TEXT,
   FEE_TEXT,
-  FOR_FUNDRAISERS_TEXT_1,
-  FOR_FUNDRAISERS_TEXT_2,
+  FUNDRAISERS_STEP1_TEXT_1,
+  FUNDRAISERS_STEP1_TEXT_2,
+  FUNDRAISERS_STEP2_TEXT,
   REFUND_REQUEST_TEXT,
+  REFUND_TEXT,
   WHAT_IS_AURORA_TEXT,
+  WITHDRAW_TEXT,
 } from 'src/data/top'
 import { Link } from 'src/elements/Link'
 import { primaryColor, white } from 'src/styles/colors'
-import { fontWeightBold, fontWeightSemiBold } from 'src/styles/font'
+import {
+  fontWeightBold,
+  fontWeightMedium,
+  fontWeightSemiBold,
+} from 'src/styles/font'
 import { breakpoint } from 'src/styles/mixins'
 import { DISCLAIMER, RAISE } from 'src/utils/router'
 import styled from 'styled-components'
@@ -24,22 +32,8 @@ export const Top: VFC = () => (
           <div>
             <h2>What is Aurora?</h2>
             <p>{WHAT_IS_AURORA_TEXT}</p>
-          </div>
-          <div>
-            <h2>For Fundraisers</h2>
             <p>
-              {FOR_FUNDRAISERS_TEXT_1}
-              <Link href={RAISE}>
-                <a>Get Started</a>
-              </Link>
-              {FOR_FUNDRAISERS_TEXT_2}
-            </p>
-            <ul>
-              <li>Passed 72 hours since published.</li>
-              <li>Got 100,000 donations.</li>
-            </ul>
-            <p>
-              Note: Publisher must understand{' '}
+              Note: You must understand{' '}
               <a target="_blank" href={DISCLAIMER} rel="noreferrer">
                 Disclaimer
               </a>
@@ -47,16 +41,44 @@ export const Top: VFC = () => (
             </p>
           </div>
           <div>
-            <h2>For Donor</h2>
-            <h3>Donation</h3>
+            <h2>1. Creating the project</h2>
+            <h3>Fundraisers</h3>
+            <p>
+              {FUNDRAISERS_STEP1_TEXT_1}
+              <Link href={RAISE}>
+                <a>Get Started</a>
+              </Link>
+              {FUNDRAISERS_STEP1_TEXT_2}
+            </p>
+            <ul id="the-end-of-project-period">
+              <li>Passed 72 hours since published.</li>
+              <li>Got 100,000 donations.</li>
+            </ul>
+          </div>
+          <div>
+            <h2>2. During the project</h2>
+            <h3>Fundraisers</h3>
+            <p>{FUNDRAISERS_STEP2_TEXT}</p>
+            <h3>Donor</h3>
+            <h4>Donation</h4>
             <p>{DONATION_TEXT}</p>
-            <h3>Cancel</h3>
+            <h4>Cancel</h4>
             <p>{CANCEL_TEXT}</p>
-            <h3>Refund request</h3>
+          </div>
+          <div>
+            <h2>3. After the project ends</h2>
+            <p>{AFTER_PROJECT_ENDS_TEXT}</p>
+            <h3>Fundraisers</h3>
+            <h4>Refund</h4>
+            <p>{REFUND_TEXT}</p>
+            <h4>Withdraw</h4>
+            <p>{WITHDRAW_TEXT}</p>
+            <h3>Donor</h3>
+            <h4>Request Refund</h4>
             <p>{REFUND_REQUEST_TEXT}</p>
           </div>
           <div>
-            <h2>Fee</h2>
+            <h2>4. Fee</h2>
             <p>{FEE_TEXT}</p>
           </div>
         </Section>
@@ -81,6 +103,10 @@ const Section = styled.section`
     margin-bottom: 18px;
     font-size: 36px;
     font-weight: ${fontWeightSemiBold};
+  }
+  h4 {
+    font-size: 24px;
+    font-weight: ${fontWeightMedium};
   }
   p {
     margin-bottom: 24px;
@@ -112,8 +138,11 @@ const Section = styled.section`
       margin-bottom: 18px;
     }
     h3 {
+      font-size: 24px;
+      margin-bottom: 10px;
+    }
+    h4 {
       font-size: 18px;
-      margin-bottom: 8px;
     }
     p {
       margin-bottom: 24px;
--- src/data/top.ts
+++ src/data/top.ts
@@ -1,20 +1,32 @@
 export const WHAT_IS_AURORA_TEXT =
   'Aurora is a decentralized collaborative giving marketplace.\r\nWe are committed to open source software and building on the decentralized web.'
 
-export const FOR_FUNDRAISERS_TEXT_1 =
+export const FUNDRAISERS_STEP1_TEXT_1 =
   'Aurora empowers you to raise money for a project easily. Just click '
 
-export const FOR_FUNDRAISERS_TEXT_2 =
+export const FUNDRAISERS_STEP1_TEXT_2 =
   ' and create your awesome project. Once a project is published, it cannot be closed until the end of the project period. In a situation where one of the following conditions, the project period ends.'
+
+export const FUNDRAISERS_STEP2_TEXT =
+  'The key to a project success is sharing your project. There is no search page on Aurora, so you need share your url to others as many as possible.'
 
 export const DONATION_TEXT =
   'Anyone who has ETH can donate to the project you like, and get the receipt NFT. It is allowed you to donate only once per project.'
 
 export const CANCEL_TEXT =
-  'If you would like to cancel your donation, you would receive donated ETH by burning your reciept NFT. After the end of project period, you should request refund, not cancel.'
+  'If you would like to cancel your donation, you would take back donated ETH by burning your reciept NFT. If the project has already end, you should request refund, not cancel.'
 
 export const REFUND_REQUEST_TEXT =
-  'If you would like to request refund, you could request the fundraisers by burning your reciept NFT. You may or may not get the refund. It depends on the fundraisers. \r\nNote: Whether you may get refund or not, you lose the receipt NFT.'
+  'If you would like to request refund, you could request the fundraisers. You may or may not get the refund. It depends on the fundraisers. When you get refund, you lose the receipt NFT instead. \r\nNote: Once you request refund, it is not allowed you to cancel it.'
+
+export const REFUND_TEXT =
+  'You may receive requests of refund as NFT from donors. It is not always necessary to respond. When you refund a donated ETH, you can do that from lists of refund request on the project page. After refund, the refund request NFT you have will be burned.'
+
+export const WITHDRAW_TEXT =
+  'You can withdraw donated ETH only after the project ends.'
+
+export const AFTER_PROJECT_ENDS_TEXT =
+  'The image, title, and description on the project page will hide 72 hours after the project published.'
 
 export const FEE_TEXT =
   'Aurora DAO receive a 10% fee per project. It will be charged when the fundraisers withdraw donated ETH.'
