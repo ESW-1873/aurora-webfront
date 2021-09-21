678198d683372961cfb90f2cc3d8aeff3449b4da
Shuto kikuchi <mizoren8@gmail.com>
2021-09-21 15:10:43 +0900

    update: not showing pj page at dev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Router from 'next/router'
+import React from 'react'
+import { Top } from 'src/compositions/Top'
+import { isProd } from 'src/utils/env'
 import { isClient } from 'src/utils/typeguard'
 
 //TODO: 10/1になったらaboutの中身に差し替える
 //--------------------------------
 const TopPage = () => {
   if (!isClient()) return <></>
+  if (!isProd) return <Top></Top>
 
   const preferredLang =
     window.navigator.language || window.navigator.languages[0]
